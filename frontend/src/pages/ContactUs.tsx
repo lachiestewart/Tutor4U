3a444204b45c845b28070587e381c95b024db31b
lst117 <lst117@uclive.ac.nz>
2024-01-18 18:04:16 +1300

    brought changes over from dhiwise branch

--- frontend/src/pages/ContactUs.tsx
+++ frontend/src/pages/ContactUs.tsx
@@ -1,109 +1,126 @@
-import React from "react";
+import { useState } from "react";
 
 import { Button, Img, Input, Text } from "components";
 import NavBar from "components/NavBar";
 
-const ContactUs: React.FC = () => {
+interface MessageForm {
+  fname: string;
+  lname: string;
+  email: string;
+  role: string;
+  message: string;
+};
+
+const ContactUs = () => {
+
+  const [messageForm, setMessageForm] = useState<MessageForm>({
+    fname: "",
+    lname: "",
+    email: "",
+    role: "",
+    message: ""
+  });
+  const [formComplete, setFormComplete] = useState<boolean>(false);
+
+  
+  const handleChange = (field: keyof MessageForm, value: string) => {
+    /** Updates the state of messageForm when a change is made to an input */
+    setMessageForm(prevState => ({ ...prevState, [field]: value }));
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    /** Handles validation and updating the form */
+    e.preventDefault();
+    const validForm: boolean = Object.keys(messageForm).map((key: string) => messageForm[key]).every(str => str !== "");
+    if (!validForm) return;
+    console.log("messageForm", messageForm);
+    setFormComplete(true);
+    //send message to backend or email
+  }
+
   return (
     <>
-      <div className="mx-auto flex w-auto flex-col items-start justify-start gap-2.5 bg-gray-200 font-montserrat md:w-full sm:w-full">
-        <NavBar className="flex w-full items-center justify-between bg-blue_gray-700 px-[60px] py-[27px] md:flex-col md:gap-2.5 md:px-5" />
-        <div className="flex w-full flex-col items-center justify-start px-[54px] py-[30px] md:px-10 sm:px-5">
-          <div className="flex w-auto flex-col items-center justify-center rounded-[10px] bg-gray-200_01 px-[50px] py-5 md:px-5 sm:w-full">
-            <div className="flex w-auto flex-col items-center justify-start gap-2.5 py-5 sm:w-full">
-              <Img
-                className="h-[60px] w-[241px] object-cover md:h-auto sm:w-full"
-                src="images/img_union21.png"
-                alt="unionTwentyOne"
-              />
-              <Text
-                className="w-[254px] text-center text-lg text-black-900"
-                size="txtMontserratRomanSemiBold18"
+      <NavBar />
+      <div className="bg-gray-200 flex flex-col font-montserrat items-center mx-auto w-auto sm:w-full md:w-full">
+
+        <div className="bg-gray-200_01 md:px-5 px-[50px] py-5 rounded-[10px] w-auto sm:w-full">
+          <div className="flex flex-col gap-2.5 items-center justify-start py-5 w-auto sm:w-full">
+            <Img
+              className="h-[60px] md:h-auto object-cover w-[241px] sm:w-full"
+              src="images/img_union21.png"
+              alt="unionTwentyOne"
+            />
+            <Text
+              className="text-black-900 text-center text-lg w-[254px]"
+              size="txtMontserratRomanSemiBold18"
+            >
+              We’d love to hear from you!
+            </Text>
+            <Text
+              className="max-w-[315px] md:max-w-full text-black-900 text-center text-sm"
+              size="txtMontserratRomanMedium14"
+            >
+              Fill in the details below and one of our team will get back to
+              you as soon as possible :)
+            </Text>
+
+            {!formComplete ?
+              <form
+                className="flex flex-col gap-2.5 items-center justify-start w-full"
+                onSubmit={handleSubmit}
               >
-                We’d love to hear from you!
-              </Text>
-              <Text
-                className="max-w-[315px] text-center text-sm text-black-900 md:max-w-full"
-                size="txtMontserratRomanMedium14"
-              >
-                Fill in the details below and one of our team will get back to
-                you as soon as possible :)
-              </Text>
-              <div className="flex w-full flex-col items-center justify-start gap-2.5">
-                <div className="flex w-full flex-row items-start justify-start gap-2.5 sm:flex-col">
-                  <div className="flex w-full flex-1 flex-col items-start justify-start">
-                    <Text
-                      className="w-auto text-center text-lg text-black-900"
-                      size="txtMontserratRomanSemiBold18"
-                    >
-                      First Name
-                    </Text>
-                    <Input
-                      name="emailtextentry"
-                      placeholder=""
-                      className="w-full p-0"
-                      wrapClassName="flex h-[50px] w-full"
-                    ></Input>
-                  </div>
-                  <div className="flex w-full flex-1 flex-col items-start justify-start">
-                    <Text
-                      className="w-auto text-center text-lg text-black-900"
-                      size="txtMontserratRomanSemiBold18"
-                    >
-                      Last Name
-                    </Text>
-                    <Input
-                      name="emailtextentry_One"
-                      placeholder=""
-                      className="w-full p-0"
-                      wrapClassName="flex h-[50px] w-full"
-                    ></Input>
-                  </div>
-                </div>
-                <div className="flex w-[476px] flex-col items-start justify-start sm:w-full">
-                  <Text
-                    className="w-auto text-center text-lg text-black-900"
-                    size="txtMontserratRomanSemiBold18"
-                  >
-                    Email
-                  </Text>
-                  <Input
-                    name="emailtextentry_Two"
-                    placeholder=""
-                    className="w-full p-0"
-                    wrapClassName="flex h-[50px] w-full"
-                  ></Input>
-                </div>
-                <div className="flex w-[476px] flex-col items-start justify-start sm:w-full">
-                  <Text
-                    className="max-w-[476px] text-lg text-black-900 md:max-w-full"
-                    size="txtMontserratRomanSemiBold18"
-                  >
-                    Just so we have context and we can answer your query
-                    appropriately, are you a....
-                  </Text>
-                  <div className="h-[50px] w-full rounded-[10px] bg-white-A700"></div>
-                </div>
-                <div className="flex w-[476px] flex-col items-start justify-start sm:w-full">
-                  <Text
-                    className="w-auto text-center text-lg text-black-900"
-                    size="txtMontserratRomanSemiBold18"
-                  >
-                    Message
-                  </Text>
-                  <div className="h-[134px] w-full rounded-[10px] bg-white-A700"></div>
-                </div>
-                <Button
-                  className="min-w-[83px] cursor-pointer text-center text-sm font-medium leading-[normal]"
-                  shape="round"
-                  color="blue_gray_700"
-                  size="sm"
-                  variant="fill"
-                >
-                  Submit
-                </Button>
-              </div>
-            </div>
+
+                <Input
+                  label="First Name"
+                  onChange={e => handleChange('fname', e)}
+                  placeholder="first name"
+                  className="w-full"
+                  wrapClassName="flex h-[50px] w-full"
+                ></Input>
+
+                <Input
+                  label="Last Name"
+                  onChange={e => handleChange('lname', e)}
+                  placeholder="last name"
+                  className="w-full"
+                  wrapClassName="flex h-[50px] w-full"
+                ></Input>
+
+                <Input
+                  label="Email"
+                  onChange={e => handleChange('email', e)}
+                  placeholder="email address"
+                  className="w-full"
+                  wrapClassName="flex h-[50px] w-full"
+                ></Input>
+
+                <Input
+                  label="For context are you a..."
+                  onChange={e => handleChange('role', e)}
+                  placeholder="student, tutor, parent ..."
+                  className="w-full"
+                  wrapClassName="flex h-[50px] w-full"
+                ></Input>
+
+                <Input
+                  label="Message"
+                  onChange={e => handleChange('message', e)}
+                  placeholder="message"
+                  className="w-full"
+                  wrapClassName="flex h-[130px] w-full"
+                ></Input>
+
+
+                <button
+                  className="font-medium w-1/2 min-w-[83px] py-3 text-center text-sm rounded-[10px] bg-blue_gray-700 text-white-A700 p-[5px]"
+                  type="submit"
+                >Submit</button>
+
+              </form>
+
+              : <Text>
+                Thank you for submitting a message to the Tutor4U team, we will be in touch shortly
+              </Text>}
           </div>
         </div>
       </div>
