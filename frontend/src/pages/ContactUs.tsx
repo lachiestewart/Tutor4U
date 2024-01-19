b2eab0b5f852e6e27afe905ff0d7d29dd10e52f6
Jason <jasonhartnz@gmail.com>
2024-01-19 21:53:26 +1300

    Completed display settings for the contact us page

--- frontend/src/pages/ContactUs.tsx
+++ frontend/src/pages/ContactUs.tsx
@@ -41,87 +41,75 @@ const ContactUs = () => {
   return (
     <>
       <NavBar />
-      <div className="mx-auto flex w-auto flex-col items-center bg-gray-200 font-montserrat md:w-full sm:w-full">
-        <div className="w-auto rounded-[10px] bg-gray-200_01 px-[50px] py-5 md:px-5 sm:w-full">
-          <div className="flex w-auto flex-col items-center justify-start gap-2.5 py-5 sm:w-full">
-            <img
-              className="h-[60px] w-[241px] object-cover md:h-auto sm:w-full"
-              src="images/img_union21.png"
-              alt="unionTwentyOne"
-            />
-            <Text
-              className="w-[254px] text-center text-lg text-black-900"
-              size="txtMontserratRomanSemiBold18"
+      <div className="mx-auto min-h-[100vh] h-auto flex flex-col items-center justify-start rounded-[10px] bg-gray-200 py-10  px-[50px] font-montserrat md:w-full md:px-5 sm:w-full">
+        <div className="flex flex-col items-center gap-3">
+          <img
+            className="h-[60px] w-[241px] object-cover md:h-auto sm:w-full"
+            src="images/img_union21.png"
+            alt="unionTwentyOne"
+          />
+          <h4 className="pt-3"
+          >
+            We’d love to hear from you!
+          </h4>
+          <p
+          >
+            Fill in the details below and one of our team will get back to you as
+            soon as possible :)
+          </p>
+          {!formComplete ? (
+            <form
+              className="flex w-full flex-col items-center justify-start gap-3"
+              onSubmit={handleSubmit}
             >
-              We’d love to hear from you!
-            </Text>
-            <Text
-              className="max-w-[315px] text-center text-sm text-black-900 md:max-w-full"
-              size="txtMontserratRomanMedium14"
-            >
-              Fill in the details below and one of our team will get back to you
-              as soon as possible :)
-            </Text>
-
-            {!formComplete ? (
-              <form
-                className="flex w-full flex-col items-center justify-start gap-2.5"
-                onSubmit={handleSubmit}
+              <Input
+                label="First Name"
+                onChange={(e) => handleChange("fname", e)}
+                placeholder="first name"
+                className="w-full"
+                wrapClassName="flex h-[50px] w-full"
+              ></Input>
+              <Input
+                label="Last Name"
+                onChange={(e) => handleChange("lname", e)}
+                placeholder="last name"
+                className="w-full"
+                wrapClassName="flex h-[50px] w-full"
+              ></Input>
+              <Input
+                label="Email"
+                onChange={(e) => handleChange("email", e)}
+                placeholder="email address"
+                className="w-full"
+                wrapClassName="flex h-[50px] w-full"
+              ></Input>
+              <Input
+                label="For context are you a..."
+                onChange={(e) => handleChange("role", e)}
+                placeholder="student, tutor, parent ..."
+                className="w-full"
+                wrapClassName="flex h-[50px] w-full"
+              ></Input>
+              <Input
+                label="Message"
+                onChange={(e) => handleChange("message", e)}
+                placeholder="Let us know how we can help you :)"
+                className="w-full"
+                wrapClassName="flex items-start h-[130px] w-full"
+              ></Input>
+              <button
+                className="w-1/2 min-w-[83px] rounded-[10px] bg-blue_gray-700 p-[5px] py-3 text-center text-sm font-medium text-white-A700"
+                type="submit"
               >
-                <Input
-                  label="First Name"
-                  onChange={(e) => handleChange("fname", e)}
-                  placeholder="first name"
-                  className="w-full"
-                  wrapClassName="flex h-[50px] w-full"
-                ></Input>
-
-                <Input
-                  label="Last Name"
-                  onChange={(e) => handleChange("lname", e)}
-                  placeholder="last name"
-                  className="w-full"
-                  wrapClassName="flex h-[50px] w-full"
-                ></Input>
-
-                <Input
-                  label="Email"
-                  onChange={(e) => handleChange("email", e)}
-                  placeholder="email address"
-                  className="w-full"
-                  wrapClassName="flex h-[50px] w-full"
-                ></Input>
-
-                <Input
-                  label="For context are you a..."
-                  onChange={(e) => handleChange("role", e)}
-                  placeholder="student, tutor, parent ..."
-                  className="w-full"
-                  wrapClassName="flex h-[50px] w-full"
-                ></Input>
-
-                <Input
-                  label="Message"
-                  onChange={(e) => handleChange("message", e)}
-                  placeholder="message"
-                  className="w-full"
-                  wrapClassName="flex h-[130px] w-full"
-                ></Input>
-
-                <button
-                  className="w-1/2 min-w-[83px] rounded-[10px] bg-blue_gray-700 p-[5px] py-3 text-center text-sm font-medium text-white-A700"
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            ) : (
-              <Text>
-                Thank you for submitting a message to the Tutor4U team, we will
-                be in touch shortly
-              </Text>
-            )}
-          </div>
+                Submit
+              </button>
+            </form>
+          ) : (
+            <h4 className="py-40">
+              Thank you for submitting a message to the Tutor4U team, we will be
+              in touch shortly!
+            </h4>
+          )}
         </div>
       </div>
     </>
